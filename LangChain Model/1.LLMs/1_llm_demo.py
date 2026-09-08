aab1114e8a78658eabadb5da1c727998b99001a7
Shubham Chauhan <shubham.spc90@gmail.com>
2026-09-08 15:50:11 +0530

    git updated

---
+++ LangChain Model/1.LLMs/1_llm_demo.py
@@ -0,0 +1,44 @@
+#  #  for OPEN AI 
+
+# from langchain_openai import OpenAI
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# llm = OpenAI(model="gpt-3.5-turbo-instruct")
+# result = llm.invoke("what is the capital of INDIA ?")
+
+# print(result)
+
+
+# ================================Without API KEY======================
+
+# #  for Ollama  locally 
+# from langchain_ollama import ChatOllama
+# from dotenv import load_dotenv
+# llm = ChatOllama(
+#     model="llama3",
+#     temperature=0
+# )
+
+# result = llm.invoke("What is the capital of India?")
+
+# print(result.content)
+
+
+
+
+from langchain_ollama import OllamaLLM
+
+# Local Ollama model initialize karein
+llm = OllamaLLM(model="llama3")
+
+# Prompt run karein
+response = llm.invoke("Explain Quantum Computing in 2 sentences.")
+print(response)
+ 
+ 
+ 
+ 
+ 
+
